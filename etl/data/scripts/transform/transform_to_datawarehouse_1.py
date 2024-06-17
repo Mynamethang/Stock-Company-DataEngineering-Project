e898b8a9f95bcdc686878702e5260da243ea2c0a
Nguyễn Ngọc Thắng <109019819+Mynamethang@users.noreply.github.com>
2024-06-17 22:01:36 +0700

    Update transform_to_datawarehouse_1.py

--- etl/data/scripts/transform/transform_to_datawarehouse_1.py
+++ etl/data/scripts/transform/transform_to_datawarehouse_1.py
@@ -20,7 +20,7 @@ def process(parquet_file_path):
     df_pandas = df_spark.toPandas()
 
     # Path to DuckDB database file
-    database_path = '/home/anhcu/Project/Stock_project/datawarehouse.duckdb'
+    database_path = '/home/ngocthang/Project/Stock_project/datawarehouse.duckdb'
 
     # Connect to DuckDB
     conn = duckdb.connect(database=database_path)
